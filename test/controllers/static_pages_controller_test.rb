3dbeed5e94baeaa73d4963d075b8f78fa6cd9d2d
mingying lin <s3568499@student.rmit.edu.au>
2018-05-05 12:19:06 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,13 +6,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get help" do
-    get static_pages_help_url
+  test "should get login" do
+    get static_pages_login_url
     assert_response :success
   end
   
  test "should get submit" do
-    get static_pages_about_url
+    get static_pages_submitt_url
     assert_response :success
   end
 end
